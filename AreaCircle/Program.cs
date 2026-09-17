f45c2d991877e7bed0a58b7398058ae0adfb19f8
ludvig562 <ludvig.he07@gmail.com>
2026-09-17 16:50:48 +0200

    fix comment

--- AreaCircle/Program.cs
+++ AreaCircle/Program.cs
@@ -7,7 +7,7 @@ namespace AreaCircle
     {
         static void Main(string[] args)
         {
-            Circle circle1 = new Circle(5);// make a new argumentin the class cirkle named circle1 that has the value 5 and sends it to the method 
+            Circle circle1 = new Circle(5);// make a new object in the class cirkle named circle1 that has the value 5 and sends it to the method 
             Circle circle2 = new Circle(6);
 
             Console.WriteLine(circle1.GetArea());// call the method GetArea's result for det name circle1 and print it on the screen 
