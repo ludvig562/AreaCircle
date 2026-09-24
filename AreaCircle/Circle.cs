497fc92119161e432e2ddba0060c632e5c24f308
ludvig562 <ludvig.he07@gmail.com>
2026-09-24 19:42:33 +0200

    added file with class Circle

---
+++ AreaCircle/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaCircle
+{
+    internal class Circle
+    {
+        private int _radius;
+
+        public Circle(int radius)// konstruktor with parametern radius
+        {
+            _radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return _radius * _radius * Math.PI; // Calculate the area of the circle 
+        }
+    }
+}
--- AreaCircle/Program.cs
+++ AreaCircle/Program.cs
@@ -14,20 +14,4 @@ namespace AreaCircle
             Console.WriteLine(circle2.GetArea());
         }
     }
-
-    class Circle
-    {
-        private int _radius;
-
-        public Circle(int radius)// konstruktor with parametern radius
-        {
-            _radius = radius; 
-        }
-
-        public double GetArea()
-        {
-            return _radius * _radius * Math.PI; // Calculate the area of the circle 
-        }
-    }
-
 }
